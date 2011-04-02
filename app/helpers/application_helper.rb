1e539537a2c7a6892af3d9f73afef81e35b15c62
Michael Young <mikeyoung13@gmail.com>
2011-04-01 21:42:42 -0400

    CRUD working for Trials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
   def formatTime(date)
+    
+    if !date.nil?
+      date.in_time_zone('Eastern Time (US & Canada)').strftime('%H:%M:%S.%L')
+    else
+      ""
+    end
 
     #print ActiveSupport::TimeZone.us_zones()
     #print date.class
-    date.in_time_zone('Eastern Time (US & Canada)').strftime('%H:%M:%S.%L')
+    #date.in_time_zone('Eastern Time (US & Canada)').strftime('%H:%M:%S.%L')
     
     #date.local.strftime('%H:%M:%S.%L')
   end
